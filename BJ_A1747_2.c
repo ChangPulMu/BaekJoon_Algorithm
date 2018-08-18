d6a50e1d6a37076e4719841f026727080a6d00ce
ChangPulMu <>
2018-08-18 17:21:18 +0900

    백준 알고리즘 1747번 문제 시행 착오 파일들과 최종 파일

---
+++ BJ_A1747_2.c
@@ -0,0 +1,48 @@
+/*어떤 수와 그 수의 숫자 순서를 뒤집은 수가 일치하는 수를 팰린드롬이라 부른다. 예를 들어 79,197과 324,423 등이 팰린드롬 수이다.
+
+어떤 수 N (1 ≤ N ≤ 1,000,000)이 주어졌을 때, N보다 크거나 같고, 소수이면서 팰린드롬인 수 중에서, 가장 작은 수를 구하는 프로그램을 작성하시오.*/
+
+#include <stdio.h>
+int Ten(int a){
+	int result = 1;
+	for(int b=1;b<a;b++)
+		result *= 10;
+	return result;
+}
+
+int main(void){
+	int ans, tmp, len=1, correct=0, turn;
+
+	scanf("%d", &ans);
+
+	while(1){
+		while(1){
+			if(ans<Ten(len+1))
+				break;
+			len += 1;
+		}
+		turn = len/2;
+		tmp = len;
+		for(int I = 1;I<=turn;I++,tmp--){
+			if(turn == 1)
+				if((ans/Ten(tmp))==(ans%Ten(I+1))/Ten(I))
+					correct++;
+			else if((ans/Ten(tmp))%Ten(I)==(ans%Ten(I+1))/Ten(I))
+				correct++;
+		}
+		if(correct==turn){
+			for(tmp=2;tmp<ans;tmp++){
+				if(ans%tmp==0)
+					break;
+			}
+			if(ans==tmp){
+				printf("%d\n", ans);
+				break;
+			}
+		}
+		ans++;
+		correct = 0;
+	}
+
+	return 0;
+}
