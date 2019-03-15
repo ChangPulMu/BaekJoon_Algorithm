151a468957c3370d9964afc8c9426ab4d51aab8e
ChangPulMu <16906@naver.com>
2019-03-15 14:13:08 +0900

    백준 알고리즘 1008번 문제 class파일이 아닌 java파일로 변경

---
+++ BJ_A1008.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class BJ_A1008 
+{
+	public static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		double a, b;
+		
+		a = sc.nextDouble();
+		b = sc.nextDouble();
+		
+		System.out.println(a/b);
+	}
+}
