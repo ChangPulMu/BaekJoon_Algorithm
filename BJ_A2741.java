62af1853426c84899979762f47639e4b1648b9d8
ChangPulMu <16906@naver.com>
2019-03-24 14:51:04 +0900

    백준 알고리즘 2741번 문제 해결(Java)

---
+++ BJ_A2741.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BJ_A2741
+{
+	public static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		int num = sc.nextInt();
+		
+		for(int i=1;i<=num;i++)
+			System.out.println(i);
+	}
+}
