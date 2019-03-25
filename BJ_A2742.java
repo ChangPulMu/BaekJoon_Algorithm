f7a1efb4630ed8dd0147244ca9a6025cda457375
ChangPulMu <16906@naver.com>
2019-03-25 21:38:17 +0900

    백준 알고리즘 2742번 문제 해결(Java)

---
+++ BJ_A2742.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BJ_A2742
+{
+	public static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		int num = sc.nextInt();
+		
+		for(int i=num;i>=1;i--)
+			System.out.println(i);
+	}
+}
