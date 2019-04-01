d0de34a4b5efd7c1f98e093a53995ad7f0a94d2a
ChangPulMu <16906@naver.com>
2019-04-01 15:32:17 +0900

    백준 알고리즘 10718번 문제 해결(Java)

---
+++ BJ_A10718.java
@@ -0,0 +1,13 @@
+public class BJ_A10718 
+{
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		String s = new String();
+		
+		s = "강한친구 대한육군";
+		
+		System.out.println(s);
+		System.out.println(s);
+	}
+}
