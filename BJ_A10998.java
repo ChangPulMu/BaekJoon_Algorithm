80ecb024411e17b8a0ad846bef3abc57df475358
ChangPulMu <16906@naver.com>
2019-04-04 10:33:38 +0900

    백준 알고리즘 10998번 문제 해결(Java)

---
+++ BJ_A10998.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class BJ_A10998 
+{
+	public static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		int a, b;
+		
+		a = sc.nextInt();
+		b = sc.nextInt();
+		
+		System.out.println(a*b);
+	}
+}
