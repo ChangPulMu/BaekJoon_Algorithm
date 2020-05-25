259089c3147f200c676056d14feb4c2dbef1553a
ChangPulMu <16906@naver.com>
2020-05-26 00:15:58 +0900

    백준 알고리즘 1436번 문제 해결(C++)

---
+++ BJ_A1436.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int n, num = 0, count = 0, test;
+	
+	cin >> n;
+	
+	while(n != count)
+	{
+		num++;
+		
+		test = num;
+		
+		while((test / 10) != 0)
+		{
+			if(((test % 10) == 6) && (((test / 10) % 10) == 6) && (((test / 100) % 10) == 6)) 
+			{
+				count++;
+				break;
+			}
+			
+			test /= 10;
+		}
+	}
+	
+	cout << num;
+	
+	return 0;
+}
+
