4d2371fdc2c4d0862d58349d444ae45f3e5a0715
ChangPulMu <16906@naver.com>
2020-05-31 22:54:14 +0900

    백준 알고리즘 2798번 문제 해결(C++)

---
+++ BJ_A2798.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(void)
+{
+	int n, m, best, ans = 0;
+	
+	cin >> n >> m;
+	
+	vector<int> v(n);
+	
+	for(int i=0;i<n;i++)
+		cin >> v[i];
+	
+	sort(v.begin(), v.end());
+	
+	for(int i=n-1;i>1;i--)
+	{
+		best = v[i];
+		for(int j=i-1;j>0;j--)
+		{
+			if(best + v[j] <= m)
+			{
+				best += v[j];
+				for(int z=j-1;z>=0;z--)
+				{
+					if(best + v[z] <=m && best + v[z] > ans)
+						ans = best + v[z];
+				}
+				best = v[i];
+			}
+		}
+	}
+	
+	cout << ans;
+}
+
