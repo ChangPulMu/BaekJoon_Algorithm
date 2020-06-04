041f5c40e5e12b8062f01bee9c8796febb2df3e4
ChangPulMu <16906@naver.com>
2020-06-04 15:39:40 +0900

    백준 알고리즘 7568번 문제 해결(C++)

---
+++ BJ_A7568.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <utility>
+
+using namespace std;
+
+int main(void)
+{
+	int n;
+	
+	cin >> n;
+	
+	pair<int,int> p[n];
+	
+	for(int i=0; i<n; i++)
+		cin >> p[i].first >> p[i].second;
+	
+	for(int i=0; i<n; i++)
+	{
+		int beyond = 0;
+		
+		for(int j=0; j<n; j++)
+		{
+			if(i == j) continue;
+			if(p[i].first < p[j].first)
+				if(p[i].second < p[j].second)
+					beyond++;
+		}
+		
+		cout << beyond + 1;
+		
+		if(i != n - 1)
+			cout << " ";
+	}
+}
+
