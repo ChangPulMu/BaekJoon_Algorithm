d02cfeae818c97c8ba9e8e913ecea0b8393b4142
ChangPulMu <16906@naver.com>
2020-06-08 10:12:31 +0900

    백준 알고리즘 10870번 문제 해결(C++)

---
+++ BJ_A10870.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int Fibo(int);
+
+int main(void)
+{
+	int n;
+	cin >> n;
+	
+	cout << Fibo(n);
+}
+
+int Fibo(int num)
+{
+	if(num < 2)
+		return num;
+	else
+		return Fibo(num-1) + Fibo(num-2);
+}
