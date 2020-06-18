0d06efd2f0421064f12943e4011a0ec1a2133cad
ChangPulMu <16906@naver.com>
2020-06-18 21:23:35 +0900

    백준 알고리즘 10797번 문제 해결(C++)

---
+++ BJ_A10797.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(void)
+{
+	int n, count = 0;
+	vector<int> a(5, -1);
+	
+	cin >> n;
+	
+	for(int i = 0; i < 5; i++)
+		cin >> a[i];
+	
+	for(int i = 0; i < 5; i++)
+		if(n == a[i]) count++;
+	
+	cout << count;
+	
+	return 0;
+}
