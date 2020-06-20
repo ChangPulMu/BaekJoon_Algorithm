5b148233e6a72171daa72d0df5724780d88dc86a
ChangPulMu <16906@naver.com>
2020-06-20 09:26:30 +0900

    백준 알고리즘 10103번 문제 해결(C++)

---
+++ BJ_A10103.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int n = 100, m = 100, nt, mt, test;
+	
+	cin >> test;
+	
+	for(int i = 0; i < test; i++)
+	{
+		cin >> nt >> mt;
+		
+		if(nt > mt) m -= nt;
+		else if(nt < mt) n -= mt;
+	}
+	
+	cout << n << "\n" << m;
+	
+	return 0;
+}
