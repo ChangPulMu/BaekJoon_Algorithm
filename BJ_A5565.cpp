a36734f4f3e00a28cfe5993e2b3dbb74c1cac752
ChangPulMu <16906@naver.com>
2020-06-20 09:25:38 +0900

    백준 알고리즘 5565번 문제 해결(C++)

---
+++ BJ_A5565.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int n, tmp;
+	
+	cin >> n;
+	
+	for(int i = 0; i < 9; i++)
+	{
+		cin >> tmp;
+		n -= tmp;
+	}
+	
+	cout << n;
+	
+	return 0;
+}
