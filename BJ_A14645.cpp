dd7a852213dc499308eba32d747738abc87aa271
ChangPulMu <16906@naver.com>
2020-06-22 14:51:04 +0900

    백준 알고리즘 14645번 문제 해결(C++)

---
+++ BJ_A14645.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int n, tmp;
+
+	cin >> n >> tmp;
+
+	for (int i = 0; i < n * 2; i++)
+		cin >> tmp;
+
+	cout << "ºñ¿ÍÀÌ";
+
+	return 0;
+}
