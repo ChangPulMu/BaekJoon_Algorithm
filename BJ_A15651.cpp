cf29d2f063daca912a9d9b1d77507ceab1ee0c57
ChangPulMu <16906@naver.com>
2020-06-27 09:50:51 +0900

    백준 알고리즘 15651번 문제 해결(C++)

---
+++ BJ_A15651.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n, m;
+vector<int> v;
+
+void backTracking();
+
+int main(void)
+{
+	cin >> n >> m;
+
+	backTracking();
+}
+
+void backTracking()
+{
+	if (v.size() == m)
+	{
+		for (int i = 0; i < v.size(); i++)
+			cout << v[i] << ' ';
+		cout << "\n";
+
+		return;
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+		v.push_back(i);
+		backTracking();
+		v.pop_back();
+	}
+}
